c2b9786e5f30f764ae275639a52de0575ee9c66b
zavadzkileticia <zavadzki.leticia@escola.pr.gov.br>
2025-09-11 16:18:05 -0300

    ok 4

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ");
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
 }
