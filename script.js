551624a3430ed1659e7e916d7397b7ce13404bd9
zavadzkileticia <zavadzki.leticia@escola.pr.gov.br>
2025-09-18 14:59:40 -0300

    ok

--- script.js
+++ script.js
@@ -12,5 +12,18 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+
     return palavras;
 }
