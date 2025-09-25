96ae6b34957f8ca14bb0c2ae0596dd3d864865f8
zavadzkileticia <zavadzki.leticia@escola.pr.gov.br>
2025-09-25 15:15:24 -0300

    ok

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
     let frequencias = {};
     for (let i of palavras) {
         frequencias[i] = 0;
@@ -22,8 +40,16 @@ function processaTexto(texto) {
             }
         }
     }
+    return frequencias;
+}
 
-    console.log(frequencias);
-
-    return palavras;
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
